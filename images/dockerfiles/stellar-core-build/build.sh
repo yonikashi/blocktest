96b813e690db1e67fb026dd2dbfba9f203d05024
YoniKashi <yonikashi@gmail.com>
2019-02-23 17:42:23 +0200

    (Test-Unit-1)

--- images/dockerfiles/stellar-core-build/build.sh
+++ images/dockerfiles/stellar-core-build/build.sh
@@ -10,32 +10,8 @@ cd /stellar-core
 
 # make format
 
-wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.2.0.1227-linux.zip
-echo "Decompressing ScannerQube"
-unzip sonar-scanner-cli-3.2.0.1227-linux.zip -d .
-echo "Setting Enviroment Variable for Scanner"
-export PATH=.sonar-scanner-cli-3.2.0.1227-linux/bin:$PATH
-
-wget https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
-echo "Decompressing Build Wrapper"
-unzip build-wrapper-linux-x86.zip -d .
-echo "Setting Enviroment variable for Wrapper"
-export PATH=./build-wrapper-linux-x86:$PATH
-
-build-wrapper-linux-x86-64 --out-dir bw-output make clean all
-
-sonar-scanner \
-  -Dsonar.projectKey=blocktest \
-  -Dsonar.organization=yonikashi-github \
-  -Dsonar.sources=. \
-  -Dsonar.cfamily.build-wrapper-output=bw-output \
-  -Dsonar.host.url=https://sonarcloud.io \
-  -Dsonar.login=baba8bed9b8a116f2517dfc494139f42ae20c3b0
-
-
 # build
-#make -j $(nproc)
-
+make -j $(nproc) check && make -j $(nproc) && src/stellar-core --test
 # test
 # export ALL_VERSIONS=1
 # make check
